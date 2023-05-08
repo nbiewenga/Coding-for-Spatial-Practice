05ce56c193347ba2fbdebc520011bbb96a850a27
nbiewenga <nbiewenga@gmail.com>
2023-05-07 21:13:32 -0400

    UPDATES

--- projects/Final_Project/main.js
+++ projects/Final_Project/main.js
@@ -35,7 +35,91 @@ const islands = [
     country: "DominicanRepublic / Haiti",
     size: "large",
     image: "https://user-images.githubusercontent.com/123484377/235378297-9cf7c191-26fc-4d00-bfef-c689073ff0d0.jpg"
-  }
+  },
+  {
+    name: "Honshu", 
+    country: "Japan",
+    size: "large",
+    image: "https://user-images.githubusercontent.com/123484377/235378298-3de85710-8fed-4a02-8f80-d2f7db85a755.jpg"
+  },
+  {
+    name: "Jamaica", 
+    country: "Jamaica",
+    size: "small",
+    image: "https://user-images.githubusercontent.com/123484377/235378299-7adde0f7-6b1b-4d34-a3da-43d4944a95f0.jpg"
+  },
+  {
+    name: "Java", 
+    country: "Indonesia",
+    size: "large",
+    image: "https://user-images.githubusercontent.com/123484377/235378300-a673f48d-9087-4bd5-910d-a5437921d9ba.jpg"
+  },
+  {
+    name: "Kyushu", 
+    country: "Japan",
+    size: "medium",
+    image: "https://user-images.githubusercontent.com/123484377/235378300-a673f48d-9087-4bd5-910d-a5437921d9ba.jpg"
+  },
+  {
+    name: "Luzon", 
+    country: "Philippines",
+    size: "medium",
+    image: "https://user-images.githubusercontent.com/123484377/235378308-d42a977a-b115-4aa9-af04-773d3d877cf4.jpg"
+  },
+  {
+    name: "Mindanao", 
+    country: "Philippines",
+    size: "large",
+    image: "https://user-images.githubusercontent.com/123484377/235378307-96002e9e-44fd-411d-a0e8-283f407b4b7d.jpg"
+  },
+  {
+  name: "Negros", 
+  country: "Philippines",
+  size: "small",
+  image: "https://user-images.githubusercontent.com/123484377/235378305-1dce8dbf-053f-4670-8010-706fe714b2b9.jpg"
+  },
+  {
+    name: "Panay", 
+    country: "Philippines",
+    size: "small",
+    image: "https://user-images.githubusercontent.com/123484377/235378304-72527746-9791-4714-ac97-4d2d8fa11416.jpg"
+  },
+  {
+    name: "Puerto Rico", 
+    country: "United States",
+    size: "small",
+    image: "https://user-images.githubusercontent.com/123484377/235378303-f71c8137-54e0-44ad-b1f2-515c943e6e2c.jpg"
+  },
+  {
+    name: "Samar", 
+    country: "Philippines",
+    size: "small",
+    image: "https://user-images.githubusercontent.com/123484377/235378302-e3fbdb24-e9d2-4506-a89a-fa4d60c7f818.jpg"
+  },
+  {
+    name: "Shikoku", 
+    country: "Japan",
+    size: "small",
+    image: "https://user-images.githubusercontent.com/123484377/235378301-59654235-8f6e-4d13-a463-57f0e9b6fb4c.jpg"
+  },
+  {
+    name: "Sicily", 
+    country: "Italy",
+    size: "medium",
+    image: "https://user-images.githubusercontent.com/123484377/235378310-b7e04d81-00b4-4671-948a-252fdcab2daa.jpg"
+  },
+  {
+    name: "Sri Lanka", 
+    country: "Sri Lanka",
+    size: "large",
+    image: "https://user-images.githubusercontent.com/123484377/235378311-cb145e45-455a-41f5-ac94-0c2a8b25be3d.jpg"
+  },
+  {
+    name: "Taiwan", 
+    country: "Taiwan",
+    size: "large",
+    image: "https://user-images.githubusercontent.com/123484377/235378312-9b95b0c9-8c5e-4b7b-b7be-c1ffd7dfc8ab.jpg"
+  },
 ];
 
 //------------- RENDER ISLANDS TO PAGE
@@ -73,27 +157,33 @@ function renderIslandsToPage(results) {
 }
 renderIslandsToPage(islands);
 
-//THIS CHUNK IS FILTER BOX
-const select = document.querySelector('select')
+let filterBtns = document.querySelector(".filters");
+let cards = document.querySelectorAll(".card");
 
-function filterFn(e) {
-  console.log("clicked", e.target.value);
-  let filterValue = e.target.value
-  
-  for (let i = 0; i < cards.length; i++) {
-    if(filterValue === "small"){
-        // render yellow flowers
-      } else if(filterValue === "medium"){
-        // render red flowers
-      } else if(filterValue === "large"){
-        // render blue flowers
+function filterFn(event) {
+  console.log(event.target)
+  if(event.target.classList.contains('filter-btn')){
+    // select the current active button
+    let activeBtn = filterBtns.querySelector('.active')
+    activeBtn.classList.remove('active')
+
+    // apply the active class to the target
+    event.target.classList.add('active')
+
+    const filterValue = event.target.getAttribute('data-filter') // yellow | red
+
+    for(let i = 0; i < cards.length; i++){
+      if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
+        cards[i].classList.remove('hide')
+        cards[i].classList.add('show')
       } else {
-        // render all flowers
-      } 
-   }
+        cards[i].classList.remove('show')
+        cards[i].classList.add('hide')
+      }
+
+    }
+  }
 }
+filterBtns.addEventListener("click", filterFn);
 
-select.addEventListener('change', filterFn)
 
-
-//NEXT NEED TO CO-ORDIDNATE OLD CODE USING THE BUTTONS WITH THE DROP DOWN SO THAT IT FILTERS THE IMAGES...
